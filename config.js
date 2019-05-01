9c9321c31589046ca4fd1c24b4a193817cc6a652
Mariusz Domanski <mariusz@domanski.pl>
2019-05-01 18:02:49 +0200

    Create app

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '312613352701-s779chc4i9ngc6b6krvp5s9j5q66od53.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'CCzrVV0oUTeQIn5tUNC8z9TZ',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
